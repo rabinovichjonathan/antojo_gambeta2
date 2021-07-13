25e7853af419c75ce3007bed240ccdedc4a1c1e0
andresrabinovich <andresrabinovich@gmail.com>
2021-07-12 22:55:58 -0300

    Cambia configuracion de mysql para conectarse en heroku

--- config/config.js
+++ config/config.js
@@ -1,8 +1,10 @@
 const mysqlconfig = {
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'ba60b1dd65e723',
-    password: 'f3917b20@us',
+    password: 'f3917b20',
     database: 'heroku_a8c597d47cbb801'
 }
 
 module.exports = mysqlconfig;
+
+mysql://ba60b1dd65e723:f3917b20@us-cdbr-east-04.cleardb.com/heroku_a8c597d47cbb801?reconnect=true
